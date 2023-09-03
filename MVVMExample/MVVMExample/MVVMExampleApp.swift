8b889f01339a8d8d0e56f672df61ad2db42e8076
lugalu <luizjaf01@gmail.com>
2023-09-03 17:55:59 -0300

    Feat: first sample setup

---
+++ MVVMExample/MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,12 @@
+//Created by Lugalu on 03/09/23.
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
