2cc4673e1d85f1b84012cae82c34bfa7f8337ec8
lugalu <luizjaf01@gmail.com>
2023-10-09 18:36:35 -0300

    Feat: initial MVVMC project created

---
+++ MVVMC Example/MVVMC Example/MVVMC_ExampleApp.swift
@@ -0,0 +1,12 @@
+//Created by Lugalu on 09/10/23.
+
+import SwiftUI
+
+@main
+struct MVVMC_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
