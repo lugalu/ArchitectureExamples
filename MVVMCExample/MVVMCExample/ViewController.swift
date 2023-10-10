a1e24f4b0c4f0e052ea937511087bbc5c2186a0c
lugalu <luizjaf01@gmail.com>
2023-10-10 18:51:00 -0300

    Feat: Reconfigured project with UIKit

---
+++ MVVMCExample/MVVMCExample/ViewController.swift
@@ -0,0 +1,18 @@
+//Created by Lugalu on 10/10/23.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+
+}
+
